84096a35462627643e4d93319cf5896154ae265f
Mariusz <mariusz.kwiecien2@onet.pl>
2023-06-08 00:05:53 +0200

    Counter-JS

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 let count = 0;
 console.log("test");
-document.getElementById("DecraseButton").onclick = function() {
-    count -= 1;
-    document.getElementById("CountLabel").innerHTML = count;
-}
+document.getElementById("DecraseButton").onclick = function () {
+  count -= 1;
+  document.getElementById("CountLabel").innerHTML = count;
+};
 
-document.getElementById("ResetButton").onclick = function(){
-    count = 0;
-    document.getElementById("CountLabel").innerHTML = count;
-}
+document.getElementById("ResetButton").onclick = function () {
+  count = 0;
+  document.getElementById("CountLabel").innerHTML = count;
+};
 
-document.getElementById("IncraseButton").onclick = function(){
-    count += 1;
-    document.getElementById("CountLabel").innerHTML = count;
-}
-
-
+document.getElementById("IncraseButton").onclick = function () {
+  count += 1;
+  document.getElementById("CountLabel").innerHTML = count;
+};
